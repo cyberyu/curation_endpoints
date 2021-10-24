0fc2b15219f5a0bc8e2516cf2e0901fe82e2baa1
Shi Yu <shee.yu@gmail.com>
2021-10-24 11:36:27 -0400

    Create en_core_web_trf_endpoint.py

---
+++ pretrainedNER/en_core_web_trf_endpoint.py
@@ -0,0 +1,24 @@
+
+# Can load up models here
+import spacy
+import utils
+from spacy_get_preds import get_spacy_preds
+
+
+model = spacy.load('en_core_web_trf')
+
+def get_preds(texts):
+    """ for now texts is list of strings
+    Output: list of lists of character spans along with label for each text predictions
+    """
+    
+    return get_spacy_preds(texts, model)
+
+
+
+    
+    
+if __name__=='__main__':
+    texts = ['Calgary is hot in the July.', 'Fred went to play Soccer in the Olympic Oval yesterday.']
+    preds = get_preds(texts)
+    print(preds)
